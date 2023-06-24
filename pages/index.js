40e1ae92dcacd0bd6cb4690d5f0c3d941483cac7
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2023-06-24 14:22:40 +0000

    new text

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,5 @@
 function Home() {
-  return (
-    <>
-      <h1>Ju, complete as lacunas 🗼</h1>
-      <p>__ Universo __ __________</p>
-      <p>_ ____ __ ______ __ sincronia</p>
-      <p>Verdades _______ _______</p>
-      <p>_____ história _ ____ _______</p>
-      <p>________ __ peças ___ _______</p>
-      <p>_ _____________ encontra _ _______</p>
-      <p>_______ _____ torre _____ _ _______</p>
-      <p>__ _________ ________ radiante.</p>
-    </>
-  );
+  return <h1>😊</h1>;
 }
 
 export default Home;
