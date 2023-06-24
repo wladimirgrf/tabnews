a7a41bcd14872fd58ddfb160d8ea83a5ba9f88c8
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2023-06-24 11:43:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 function Home() {
-    return <>
-        <h1>Ju, complete as lacunas 🗼</h1>
-        <p>__ Universo __ __________</p>
-        <p>_ ____ __ ______ __ sincronia</p>
-        <p>Verdades _______ _______</p>
-        <p>_____ história _ ____ _______</p>
-        <p>________ __ peças ___ _______</p>
-        <p>_ _____________ encontra _ _______</p>
-        <p>_______ _____  torre _____ _ _______</p>
-        <p>__ _________ ________ radiante.</p>
+  return (
+    <>
+      <h1>Ju, complete as lacunas 🗼</h1>
+      <p>__ Universo __ __________</p>
+      <p>_ ____ __ ______ __ sincronia</p>
+      <p>Verdades _______ _______</p>
+      <p>_____ história _ ____ _______</p>
+      <p>________ __ peças ___ _______</p>
+      <p>_ _____________ encontra _ _______</p>
+      <p>_______ _____ torre _____ _ _______</p>
+      <p>__ _________ ________ radiante.</p>
     </>
+  );
 }
 
 export default Home;
