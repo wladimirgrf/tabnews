a5af42b7f258dc7c28152b02f89c768718caee16
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2024-09-18 23:25:42 +0000

    Endpoint `/status`: ISO 8601 + Fuso + MVC + lowerCamelCase

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,11 @@
 import database from "infra/database"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  response.status(200).json({ chave: "valor" })
+  const updatedAt = new Date().toISOString()
+
+  response.status(200).json({ 
+    updated_at: updatedAt
+  })
 }
 
 export default status
