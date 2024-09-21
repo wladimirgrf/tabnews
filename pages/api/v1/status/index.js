fcd83d77ceccc1c557a531ddc271fe93885aad2e
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2024-09-21 18:43:51 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,25 +3,25 @@ import database from "infra/database";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const maxConnectionsResult = await database.query('SHOW max_connections;');
-  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const versionResult = await database.query('SHOW server_version;');
-  const version = versionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query('SHOW max_connections;');
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const openConnectionsResult = await database.query({
-    text: 'SELECT numbackends as opened_connections FROM pg_stat_database where datname = $1',
+  const databaseOpenConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
     values: [process.env.POSTGRES_DB],
   });
-  const openedConnections = openConnectionsResult.rows[0].opened_connections;
+  const databaseOpenConnectionsValue = databaseOpenConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        max_connections: parseInt(maxConnections),
-        opened_connections: openedConnections,
-        version: version,
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenConnectionsValue,
       },
     },
   });
