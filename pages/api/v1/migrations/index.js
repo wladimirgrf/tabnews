ee52b0dcdbbdcca22b5123831ffa6d9f43329561
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-10-12 15:31:03 +0000

    fix migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -37,5 +37,7 @@ export default async function migrations(request, response) {
     response.status(200).json(migratedMigrations);
   }
 
+  await dbClient.end();
+
   return response.status(405).end();
 }
