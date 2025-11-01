e09836baa718e70ac24ed736f404882df3334fa6
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-11-01 14:58:49 -0300

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+import password from "models/password.js";
 import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
@@ -35,6 +36,7 @@ async function findOneByUsername(username) {
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
 
@@ -80,6 +82,11 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
