f47405418d7febec6850abc86fd9ed575ec196e4
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-11-07 12:51:06 -0300

    feat: implement `/api/v1/user` endpoint with session renewal

--- models/user.js
+++ models/user.js
@@ -2,6 +2,37 @@ import password from "models/password.js";
 import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
+async function findOneById(userId) {
+  const userFound = await runSelectQuery(userId);
+
+  return userFound;
+
+  async function runSelectQuery(userId) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          *
+        FROM
+          users
+        WHERE
+          id = $1
+        LIMIT
+          1
+        ;`,
+      values: [userId],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O id informado não foi encontrado no sistema.",
+        action: "Verifique se o id está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 async function findOneByUsername(username) {
   const userFound = await runSelectQuery(username);
 
@@ -191,6 +222,7 @@ async function hashPasswordInObject(userInputValues) {
 
 const user = {
   create,
+  findOneById,
   findOneByUsername,
   findOneByEmail,
   update,
