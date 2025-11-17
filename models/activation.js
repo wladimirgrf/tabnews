e1216aecd262f76e394cfb110c6af669e395bb13
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-11-17 13:10:20 -0300

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -2,8 +2,9 @@ import database from "infra/database.js";
 import email from "infra/email.js";
 import webserver from "infra/webserver.js";
 
-import { NotFoundError } from "infra/errors.js";
+import { NotFoundError, ForbiddenError } from "infra/errors.js";
 import user from "./user";
+import authorization from "./authorization";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
 
@@ -90,6 +91,15 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
@@ -118,6 +128,7 @@ const activation = {
   markTokenAsUsed,
   activateUserByUserId,
   sendEmailToUser,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default activation;
